6391d33bd0dfac39dceef412effa567bd6145acf
Sam Dunegan <sam.dunegan@sbcglobal.net>
2021-07-01 21:18:58 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -5,20 +5,37 @@
 // for practice purposes as we are sharing the same account
 const api_key = 'd771b19ef336ed8381def3a60b574464'
 
+const axios = require('axios').default;
+
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios(url);
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
-}
+  return axios(url)
+  .then(function(result){
+    return result.data;
+  })
+};
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios(url)
+  .then(function(response){
+    return response.status
+  })
+  .catch(function(error){
+    return error.response.status
+  })
+  .then(function(result){
+    return result
+  });
 }
 
 
